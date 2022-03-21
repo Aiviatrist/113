66ea80ff35bacf829ead6fe4613a565bbfe9781b
Aiviatrist <92304985+Aiviatrist@users.noreply.github.com>
2022-03-21 19:29:03 +1000

    Add files via upload

---
+++ jeve2.js
@@ -0,0 +1,10 @@
+function setup(){
+    canvas= createCanvas(640,480);
+    canvas.position(150,150);
+    video=createCapture(VIDEO);
+    video.hide();
+}
+function draw(){
+    image(video,230,150,200,200);
+    
+}
